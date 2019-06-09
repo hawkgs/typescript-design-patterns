fbc36829b059abc1df85fe9e8b967d5dba467ddb
hawkgs <hawk.dvp@gmail.com>
2019-06-09 23:27:16 +0300

    Add abstract implementation of Abstract Factory

---
+++ creational/abstract-factory/abstract.ts
@@ -0,0 +1,26 @@
+// Abstract implementation of Abstract Factory
+
+abstract class AbstractProduct {
+  abstract value: unknown;
+}
+
+interface AbstractFactory<T extends AbstractProduct> {
+  createProduct(): T;
+}
+
+class ConcreteProduct extends AbstractProduct {
+  value = 1;
+}
+
+class ConcreteFactory implements AbstractFactory<ConcreteProduct> {
+  createProduct() {
+    return new ConcreteProduct();
+  }
+}
+
+class Client {
+  factory = new ConcreteFactory();
+}
+
+// Prints: ConcreteProduct { value: 0 }
+console.log(new Client().factory.createProduct());
