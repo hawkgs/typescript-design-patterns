48434c6e5d71be4bc9cfe024bec32427bae92db6
hawkgs <hawk.dvp@gmail.com>
2019-06-11 10:06:24 +0300

    Add abstract implementation of Builder

---
+++ creational/builder/abstract.ts
@@ -0,0 +1,37 @@
+// Abstract implementation of Builder
+
+interface Builder {
+  buildPart(v: number): void;
+}
+
+class Product {
+  constructor(public value: number) {}
+}
+
+class ConcreteBuilder implements Builder {
+  private _value: number;
+
+  get result() {
+    return new Product(this._value);
+  }
+
+  buildPart(value: number) {
+    this._value = value;
+  }
+}
+
+class Director {
+  constructor(public builder: Builder) {}
+
+  construct() {
+    this.builder.buildPart(1);
+  }
+}
+
+const builder = new ConcreteBuilder();
+const director = new Director(builder);
+
+director.construct();
+
+const product = builder.result;
+console.log(product);
